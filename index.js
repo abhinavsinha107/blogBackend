ec4cd2e320d330e297e7b5ce553bf918ad565da1
Abhinav Sinha <abhinavsinha135@gmail.com>
2023-12-21 00:10:22 +0530

    Route create

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const PORT = 8000;
+const authRoutes = require('./Routes/Auth');
 require('dotenv').config();
 require('./db');
+const User = require('./Models/UserSchema')
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
-    res.json({ message: 'The API isworking' })
+    res.json({ message: 'The API is working' })
 });
 
 app.listen(PORT, () => {
