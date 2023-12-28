f4e8b3d143d7ce4627442a02223e641e910e00cc
Abhinav Sinha <abhinavsinha135@gmail.com>
2023-12-28 15:19:54 +0530

    new Frontend url added

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ const User = require("./Models/UserSchema");
 app.use(bodyParser.json());
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://blog-frontend-nine-phi.vercel.app",
+  "https://blog-frontend-7936fclkh-abhinav-sinhas-projects.vercel.app/",
 ]; // Add more origins as needed
+
+// https://blog-frontend-nine-phi.vercel.app
 
 // Configure CORS with credentials
 app.use(
